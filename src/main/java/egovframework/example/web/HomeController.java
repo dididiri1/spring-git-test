52660ed3130d37207d85dd964bf6b2353976dc35
Kangmin Kim <dididiri1@naver.com>
2026-04-30 16:15:52 +0900

    마스터 homeController 변경

--- src/main/java/egovframework/example/web/HomeController.java
+++ src/main/java/egovframework/example/web/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping(value = {"/", "/index"})
 	public String home(Model model) {
-		System.out.println("마스터 소스 수정 테스트 #2");
+		System.out.println("마스터 소스 수정 테스트 #3");
 		model.addAttribute("message", "eGovFrame 프로젝트가 정상적으로 실행되었습니다.");
 		return "index";
 	}
