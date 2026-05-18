d3935823201a4b14a4e111c9d04fafc8b62f0ea5
Kangmin Kim <dididiri1@naver.com>
2026-05-18 13:59:14 +0900

    [Kangmin Kim] chore: HomeController 중복 출력 줄 정리

--- src/main/java/egovframework/example/web/HomeController.java
+++ src/main/java/egovframework/example/web/HomeController.java
@@ -9,11 +9,6 @@ public class HomeController {
 
 	@RequestMapping(value = {"/", "/index"})
 	public String home(Model model) {
-		System.out.println("work2 : 충돌 테스트!");
-		System.out.println("work2 : 충돌 테스트!");
-		System.out.println("work1 : 테스트!");
-		System.out.println("work1 : 테스트!");
-		System.out.println("work1 : 충돌 테스트!");
 		System.out.println("work1 : git 테스트 #1");
 		model.addAttribute("message", "eGovFrame 프로젝트가 정상적으로 실행되었습니다.");
 		return "index";
